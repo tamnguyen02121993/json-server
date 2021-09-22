00fea69f349f4a679892970dc93866f6c36e1438
tamnguyen <tamnguyen02121993@gmail.com>
2021-09-22 11:00:38 +0700

    Change port

--- main.js
+++ main.js
@@ -45,6 +45,7 @@ router.render = (req, res) => {
 
 // Use default router
 server.use(router);
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
     console.log('JSON Server is running');
 });
